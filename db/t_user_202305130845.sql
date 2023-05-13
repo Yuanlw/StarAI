93873dc20ab6356313d4bdf0111a18eb7edcd492
yuanliwei <yuanliwei@daojia-inc.com>
2023-05-13 08:48:42 +0800

    测试数据

---
+++ db/t_user_202305130845.sql
@@ -0,0 +1,16 @@
+INSERT INTO starAI.t_user (name,user_password,mail,business_type,create_time,update_time) VALUES
+	 ('Alice','123456','alice@example.com',1,'2023-05-11 10:00:00','2023-05-11 10:00:00'),
+	 ('Chris','123456','chris@example.com',1,'2023-05-10 10:00:00','2023-05-10 10:00:00'),
+	 ('example','WcvlHj','example@gmai.com',0,'2023-05-12 14:37:36','2023-05-12 14:37:36'),
+	 ('123456','','123456@gmai.com',0,'2023-05-12 14:47:21','2023-05-12 14:47:21'),
+	 ('yuanlw','','yuanlw@gmai.com',0,'2023-05-12 14:53:27','2023-05-12 14:53:27'),
+	 ('yuanlw1','','yuanlw1@gmai.com',0,'2023-05-12 14:57:11','2023-05-12 14:57:11'),
+	 ('yuanlw2','','yuanlw2@gmai.com',0,'2023-05-12 14:58:33','2023-05-12 14:58:33'),
+	 ('chris1','gXQmbA','chris1@gmai.com',0,'2023-05-12 15:02:32','2023-05-12 15:02:32'),
+	 ('chris2','','chris2@gmai.com',0,'2023-05-12 15:03:54','2023-05-12 15:03:54'),
+	 ('chris3','','chris3@gmai.com',0,'2023-05-12 15:11:53','2023-05-12 15:11:53');
+INSERT INTO starAI.t_user (name,user_password,mail,business_type,create_time,update_time) VALUES
+	 ('chris9','','chris9@gmai.com',0,'2023-05-12 15:18:30','2023-05-12 15:18:30'),
+	 ('chris10','','chris10@gmai.com',0,'2023-05-12 15:20:29','2023-05-12 15:20:29'),
+	 ('chris001','','chris001@gmai.com',0,'2023-05-12 15:33:10','2023-05-12 15:33:10'),
+	 ('chris','IwiFbq','chris@gmai.com',0,'2023-05-12 16:31:08','2023-05-12 16:31:08');
