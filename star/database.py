cd58c757709e92f2840b450f4ae5c94262739589
yuanliwei <yuanliwei@daojia-inc.com>
2023-05-30 10:19:49 +0800

    解决数据库断链问题

--- star/database.py
+++ star/database.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import create_engine, event
 
 # 创建数据库引擎和 ORM 操作类
@@ -8,25 +7,23 @@ engine = create_engine(
     pool_size=5,
     max_overflow=10,
     pool_pre_ping=True,
-    pool_recycle=300,
-    isolation_level='READ COMMITTED',
+    pool_recycle=1800,
+    isolation_level='REPEATABLE READ',
     connect_args={
         'read_timeout': 30,
         'write_timeout': 30,
         'charset': 'utf8',
-        'sql_mode': 'STRICT_TRANS_TABLES'
+        'sql_mode': 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION'
     },
     echo=True
 )
 
+
 @event.listens_for(engine, 'checkout')
 def ping_connection(dbapi_connection, connection_record,
-          connection_proxy):
-   try:
-       cursor = dbapi_connection.cursor()
-       cursor.execute('SELECT 1')
-   except:
-       dbapi_connection.close()
-       dbapi_connection = engine.pool.connect()
-
-
+                    connection_proxy):
+    try:
+        dbapi_connection.ping()
+    except:
+        dbapi_connection.close()
+        dbapi_connection = engine.pool.connect()
